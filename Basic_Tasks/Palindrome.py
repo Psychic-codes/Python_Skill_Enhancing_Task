8c6d5822380de302a89df4bba3048e57ac748a99
Om yadav <157380214+Psychic-codes@users.noreply.github.com>
2025-01-02 22:15:36 +0530

    Delete Basic_Tasks directory

--- Basic_Tasks/Palindrome.py
+++
@@ -1,8 +0,0 @@
-def palindrome(t) :
-    if t == text[::-1]:
-        print(f"The Input {t} is  Palindrome.")
-    else :
-        print(f"The Input {t} is not a Palindrome.")
-
-text = input("Enter input for Palindrome check: ")
-palindrome(text)
